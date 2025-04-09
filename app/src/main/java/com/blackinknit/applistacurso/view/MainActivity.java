b828f8506c546ca6c082d37b5aa31a526be1e352
v1ctrmourao <v1ctrhmourao@gmail.com>
2025-04-08 23:54:15 -0400

    finalização da parte model e view, partindo para controller

--- app/src/main/java/com/blackinknit/applistacurso/view/MainActivity.java
+++ app/src/main/java/com/blackinknit/applistacurso/view/MainActivity.java
@@ -1,6 +1,11 @@
 package com.blackinknit.applistacurso.view;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +24,15 @@ public class MainActivity extends AppCompatActivity {
     String dadosPessoa;
     String dadosOutraPessoa;
 
+    EditText nomeEditText;
+    EditText sobrenomeEditText;
+    EditText cursoEditText;
+    EditText telefoneEditText;
+
+    Button limparButton;
+    Button finalizarButton;
+    Button salvarButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,17 +45,65 @@ public class MainActivity extends AppCompatActivity {
         });
 
         pessoa = new Pessoa();
-        pessoa.setNome("victor");
+       /* pessoa.setNome("victor");
         pessoa.setSobreNome("Mourão");
         pessoa.setCursoDesejado("análise e desenvolvimento de sistemas");
         pessoa.setTelefone("991248807");
-
+*/
         Pessoa outraPessoa = new Pessoa();
-        outraPessoa.setNome("lucas");
+        /*outraPessoa.setNome("lucas");
         outraPessoa.setSobreNome("cavalcante");
         outraPessoa.setCursoDesejado("análise e desenvolvimento de sistemas");
-        outraPessoa.setTelefone("95991454553");
+        outraPessoa.setTelefone("95991454553");*/
 
+        //
+        nomeEditText = findViewById(R.id.nomeEditText);
+        sobrenomeEditText = findViewById(R.id.sobrenomeEditText);
+        cursoEditText = findViewById(R.id.cursoEditText);
+        telefoneEditText = findViewById(R.id.telefoneEditText);
+
+        salvarButton = findViewById(R.id.salvarButton);
+        limparButton = findViewById(R.id.limparButton);
+        finalizarButton = findViewById(R.id.finalizarButton);
+
+        //setText
+        nomeEditText.setText(pessoa.getNome());
+        sobrenomeEditText.setText(pessoa.getSobreNome());
+        cursoEditText.setText(pessoa.getCursoDesejado());
+        telefoneEditText.setText(pessoa.getTelefone());
+
+        //métodos botões
+        limparButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                nomeEditText.setText("");
+                sobrenomeEditText.setText("");
+                cursoEditText.setText("");
+                telefoneEditText.setText("");
+            }
+        });
+
+        finalizarButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "finalizado", Toast.LENGTH_LONG).show();
+                finish();
+            }
+        });
+
+        salvarButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pessoa.setNome(nomeEditText.getText().toString());
+                pessoa.setSobreNome(sobrenomeEditText.getText().toString());
+                pessoa.setCursoDesejado(cursoEditText.getText().toString());
+                pessoa.setTelefone(telefoneEditText.getText().toString());
+
+                Toast.makeText(MainActivity.this, "Salvo"+ pessoa.toString(), Toast.LENGTH_LONG).show();
+            }
+        });
+
+/*
 
         dadosPessoa = "primeiro nome: ";
         dadosPessoa += pessoa.getNome();
@@ -60,6 +122,11 @@ public class MainActivity extends AppCompatActivity {
         dadosOutraPessoa += outraPessoa.getTelefone();
         dadosOutraPessoa += " curso";
         dadosOutraPessoa += outraPessoa.getCursoDesejado();
+*/
+
+
+        Log.i("PooAndroid", pessoa.toString());
+        Log.i("PooA2", outraPessoa.toString());
 
     }
 
